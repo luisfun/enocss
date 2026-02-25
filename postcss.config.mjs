3c53f3483f859f004273f8ec2bf27e18661b42b8
Luis <101060519+LuisFun@users.noreply.github.com>
2026-02-25 17:34:30 +0900

    1

---
+++ postcss.config.mjs
@@ -0,0 +1,13 @@
+import postcssImport from 'postcss-import'
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
+
+export default {
+  plugins: [
+    postcssImport(),
+    autoprefixer(),
+    cssnano({
+      preset: 'default',
+    }),
+  ],
+}
