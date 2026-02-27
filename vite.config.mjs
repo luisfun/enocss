a4997b1328ff6192882b2b87b79b10c438f9d3fe
Luis <101060519+LuisFun@users.noreply.github.com>
2026-02-27 19:58:17 +0900

    fix: build and button.css

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 
+import { resolve } from 'path'
+import { readdirSync } from 'fs'
+
+const root = 'playground'
+const input = {}
+readdirSync(root).filter(f => f.endsWith('.html')).forEach(f => {
+  const name = f.replace(/\.html$/, '')
+  input[name] = resolve(root, f)
+})
+
 export default defineConfig({
-  root: 'playground',
+  root,
   build: {
     outDir: '../playground-preview',
     emptyOutDir: true,
+    rollupOptions: {
+      input,
+    },
   },
   preview: {
     port: 5173,
